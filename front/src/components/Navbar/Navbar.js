6e40f8cd57e2b7e8de9609af3745ac3e6e507b60
saifdg <saifdg.dghais@gmail.com>
2022-07-14 19:07:43 +0100

    a0

--- front/src/components/Navbar/Navbar.js
+++ front/src/components/Navbar/Navbar.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import "./Navbar.css"
-
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+
   return (
-    <div>
-      <button class="btn btn-primary" type="submit">Button</button> 
+    <div className="section">
+      <div className="containere">
+        <div className="logo">
+          
+         <Link to={"/"} ><img src={logo} alt="logo" /></Link>
+        </div>
+        <div className="links">
+          <Link className="a" to={"/"}>Home</Link>
+          <Link className="a" to={"/"}>Store</Link>
+          <Link className="a" to={"/"}>About Us</Link>
+        </div>
+        <div className="auth">
+          <Link className="a" to={"/"}>Sign In</Link>
+          <Link className="a adjust" to={"/"}>Register</Link>
+        </div>
+      </div>
     </div>
+  );
+};
 
-  )
-}
-
-export default Navbar
+export default Navbar;
