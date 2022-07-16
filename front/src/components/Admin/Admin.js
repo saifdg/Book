09a82527dba64f3691ad85a1bf0020a94e2a8313
saifdg <saifdg.dghais@gmail.com>
2022-07-16 04:13:41 +0100

    modifffff

---
+++ front/src/components/Admin/Admin.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import "./Admin.css";
+import Axios from "axios"
+
+const Admin = () => {
+
+    const [users , setUsers]=useState({})
+  useEffect(async () => {
+    const result = await Axios.get("/api/users");
+
+    setUsers({ location: result.data });
+
+  }, []);
+
+  console.log(users)
+  return (
+    <div className="cont">
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Admin;
